5e40a90a3adf29b0eddb7f8d906c446fcef8beb7
Dmytro Sokil <dmytrosokil@gmail.com>
2017-11-18 17:06:43 +0200

    implement pirson correlation coefficient

--- src/Correlation/Calculator/EuclideanDistanceCoefficientCalculator.php
+++ src/Correlation/Calculator/EuclideanDistanceCoefficientCalculator.php
@@ -19,25 +19,30 @@ class EuclideanDistanceCoefficientCalculator implements SimilarityCoefficientCal
         array $rating1List,
         array $rating2List
     ): CorrelationCoefficient {
-        // get list of common rates
-        $squareSumList = [];
+        $squareSum = 0;
+        $commonParametersCount = 0;
         foreach ($rating1List as $parameterName => $rating1ParameterValue)
         {
+            // accept only parameters in both lists
             if (empty($rating2List[$parameterName])) {
                 continue;
             }
 
-            $squareSumList[] = pow(
-                $rating1ParameterValue - $rating2List[$parameterName],
+            $commonParametersCount++;
+
+            $rating2ParameterValue = $rating2List[$parameterName];
+
+            $squareSum += pow(
+                $rating1ParameterValue - $rating2ParameterValue,
                 2
             );
         }
 
         // if no common rates, correlation is 0
-        if (count($squareSumList) === 0) {
+        if ($commonParametersCount === 0) {
             $coefficient = 0;
         } else {
-            $coefficient = 1 / (1 + sqrt(array_sum($squareSumList)));
+            $coefficient = 1 / (1 + sqrt($squareSum));
         }
 
         return new CorrelationCoefficient($coefficient);
--- src/Correlation/Calculator/PirsonCorrelationCoefficientCalculator.php
+++ src/Correlation/Calculator/PirsonCorrelationCoefficientCalculator.php
@@ -19,5 +19,54 @@ class PirsonCorrelationCoefficientCalculator implements SimilarityCoefficientCal
         array $rating2List
     ): CorrelationCoefficient {
 
+        $rating1Sum = 0;
+        $rating2Sum = 0;
+
+        $rating1SquareSum = 0;
+        $rating2SquareSum = 0;
+
+        $ratingMultiplicationSum = 0;
+
+        $commonParametersCount = 0;
+
+        foreach ($rating1List as $parameterName => $rating1ParameterValue) {
+            // accept only parameters in both lists
+            if (empty($rating2List[$parameterName])) {
+                continue;
+            }
+
+            $commonParametersCount++;
+
+            $rating2ParameterValue = $rating2List[$parameterName];
+
+            $rating1Sum += $rating1ParameterValue;
+            $rating2Sum += $rating2ParameterValue;
+
+            $rating1SquareSum += pow($rating1ParameterValue, 2);
+            $rating2SquareSum += pow($rating2ParameterValue, 2);
+
+            $ratingMultiplicationSum += $rating1ParameterValue * $rating2ParameterValue;
+        }
+
+        if ($commonParametersCount === 0) {
+            $coefficient = 0;
+        } else {
+            $num = $ratingMultiplicationSum - ($rating1Sum * $rating2Sum / $commonParametersCount);
+            $den = sqrt(
+                (
+                    $rating1SquareSum - pow($rating1Sum, 2) / $commonParametersCount
+                ) * (
+                    $rating2SquareSum - pow($rating2Sum, 2) / $commonParametersCount
+                )
+            );
+
+            if ($den === 0) {
+                $coefficient = 0;
+            } else {
+                $coefficient = $num / $den;
+            }
+        }
+
+        return new CorrelationCoefficient($coefficient);
     }
 }
